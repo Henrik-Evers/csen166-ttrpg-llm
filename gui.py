c797ebe80e6a401b03df4b6b404d04c3d6f22987
Henrik-Evers <hevers.de@mysummitps.org>
2025-05-28 20:24:55 -0700

    GUI (mostly) done
    Ready to start integrating into the app!

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.ttk import *
 from tkinter.filedialog import askopenfilename
+from tkinter.scrolledtext import ScrolledText
 import os
 import shutil
 
@@ -14,18 +15,10 @@ root = Tk(className=' LLM TTRPG')
 frm_menu = None
 frm_play = None
 files = {}
-# filename = askopenfilename()
 
 
 def menu():
     global frm_menu, frm_play
-
-    if frm_play is not None:
-        frm_play.grid_forget()
-
-    if frm_menu is not None:
-        frm_menu.grid()
-        return
 
     frm_menu = Frame(root, padding=10)
     frm_menu.grid()
@@ -37,37 +30,39 @@ def menu():
 
     Button(frm_menu, text="play", command=lambda: play()).grid(column=0, row=1)
 
-    def add_file(sub_dir):
+    # If full_path is present, the file already exists.
+    # sub_dir tells us which subdir of data to add to - characters, rules, or adventure.
+    def add_file(sub_dir, full_path=None):
         global files
 
-        path = askopenfilename()
+        if full_path is None:
+            path = askopenfilename()
+        else:
+            path = full_path
         if not os.path.exists(path):
             return
 
         f_path = os.path.basename(path)
-        f = {}
-        
         f_path = os.path.join(D_PATH, sub_dir, f_path)
-
-        try:
-            shutil.copy(path, f_path)
-        except:
-            return
+        
+        files[f_path] = {'path': path}
+        
 
         frm_tmp = Frame(frm_filelist, padding=10)
         frm_tmp.grid(column=0, row=len(files))
         Label(frm_tmp, text=path).grid(column=0, row=0)
         
         if f_path.endswith('.pdf'):
-            f['mode'] = 'txt'
-            f['t'] = BooleanVar()
-            Radiobutton(frm_tmp, text='text', variable=f['t'], value=False).grid(column=1, row=0)
-            Radiobutton(frm_tmp, text='img', variable=f['t'], value=True).grid(column=2, row=0)
+            files[f_path]['mode'] = 'txt'
+            files[f_path]['i'] = BooleanVar()
+            if os.path.basename(path).startswith('i'):
+                files[f_path]['i'].set(True)
+            Radiobutton(frm_tmp, text='text', variable=files[f_path]['i'], value=False).grid(column=1, row=0)
+            Radiobutton(frm_tmp, text='img', variable=files[f_path]['i'], value=True).grid(column=2, row=0)
         
         Button(frm_tmp, text="X", command=lambda: remove_file(f_path)).grid(column=3, row=0)
 
-        f['frame'] = frm_tmp
-        files[f_path] = f
+        files[f_path]['frame'] = frm_tmp
 
     def remove_file(path):
         global files
@@ -87,21 +82,59 @@ def menu():
     frm_filelist = Frame(frm_menu, padding=10)
     frm_filelist.grid(column=0, row=3)
 
+    # On load, add all files already in the folders with their current config
+    for sub_path in (RULES_PATH, ADVENTURE_PATH, CHARS_PATH):
+        for filename in os.listdir(os.path.join(D_PATH, sub_path)):
+            file_path = os.path.join(D_PATH, sub_path, filename)
+            add_file(sub_path, file_path)
+
 
 def play():
-    global frm_menu, frm_play
+    global frm_menu, frm_play, files
+
+    for f_path in files:
+        try:
+            path = f_path
+            if f_path.endswith('.pdf'):
+                ht = os.path.split(f_path)
+                if files[f_path]['i'].get() and not ht[1].startswith('i'):
+                    path = os.path.join(ht[0], 'i' + ht[1])
+                    print("1")
+                    print(path)
+                elif ht[1].startswith('i'):
+                    path = os.path.join(ht[0], ht[1][1:])
+                    print("2")
+                    print(path)
+            if files[f_path]['path'] != path:
+                shutil.copy(files[f_path]['path'], path)
+            elif os.path.realpath(os.path.dirname(os.path.dirname(files[f_path]['path']))) == os.path.realpath(D_PATH):
+                os.remove(files[f_path]['path'])
+        except Exception as e:
+            print('Could not copy: ' + files[f_path]['path'])
+            print(e)
 
     if frm_menu is not None:
         frm_menu.grid_forget()
     
-    if frm_play is not None:
-        frm_play.grid()
-        return
-    
     frm_play = Frame(root, padding=10)
     frm_play.grid()
-    Label(frm_play, text="Playing").grid(column=0, row=0)
-    Button(frm_play, text="menu", command=lambda: menu()).grid(column=1, row=0)
+    output_txt = ScrolledText(frm_play, wrap='word', width=150)
+    output_txt.grid(column=0, row=0, columnspan=2)
+    output_txt.insert(END, "This is some sample text.\n")
+    output_txt.config(state=DISABLED)
+    input_txt = ScrolledText(frm_play, wrap='word', height=4, width=150)
+    input_txt.grid(column=0, row=1)
+    Button(frm_play, text="Send", command=lambda: enter(), width=10).grid(column=1, row=1)
+
+    def enter(event=None):
+        msg = input_txt.get('1.0', 'end')
+        input_txt.delete('1.0', 'end')
+        submit_msg(msg)
+    root.bind('<Return>', enter)
+
+
+def submit_msg(msg):
+    print(msg)
 
 
 menu()
